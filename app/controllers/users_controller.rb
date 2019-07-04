8b73af2611c04a4799a84ffcaec7e7c32d6b2820
mirage <mirage@MacBook-Pro-2.local>
2019-07-04 21:49:57 +0400

    + permission and roles methods to user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :update, :destroy]
+  before_action :set_user, only: [:show, :update, :destroy, :check_permission, :grant_permissions]
+  before_action :set_roles, only: [:grant_permissions]
 
   # GET /users
   def index
@@ -38,14 +39,39 @@ class UsersController < ApplicationController
     @user.destroy
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
+  # GET send permission id in this method to check if it include in user
+  def check_permission
+    if @user.permissions.pluck(:id).include?(params[:permission_id].to_i)
+      render json: 'Permission found'
+    else
+      render json: 'Permission not found'
     end
+  end
 
-    # Only allow a trusted parameter "white list" through.
-    def user_params
-      params.require(:user).permit(:id, :name, permissions_attributes: [:id, :name, :permission_type, :role_id, :user_id])
+  # POST send role ids to this method to update roles in user
+  def grant_roles
+    @user.roles = @roles
+    if @user.save
+      render json: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
     end
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  # Find roles by role ids
+  def set_roles
+    @roles = Role.where(id: params[:role_ids])
+  end
+
+  # Only allow a trusted parameter "white list" through.
+  def user_params
+    params.require(:user).permit(:id, :name, permissions_attributes: [:id, :name, :permission_type, :role_id, :user_id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   resources :roles
-  resources :users
+  resources :users do
+    member do
+      get :check_permission
+      post :grant_permissions
+    end
+  end
 end
